51375c15162e61435fffb3f1102256b6bca046a1
Manoj Pise <110347878+manojpisepatil@users.noreply.github.com>
2026-01-04 22:08:34 +0530

    Update wsgi.py

--- api/wsgi.py
+++ api/wsgi.py
@@ -1,2 +1,14 @@
-from internship.wsgi import application as app
+# from internship.wsgi import application as app
+# app = application
+
+import os
+from internship.wsgi import application
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'internship.settings')
+
+application = get_wsgi_application()
+
+
 app = application
